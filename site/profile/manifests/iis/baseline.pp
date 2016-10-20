bd1a515876994f1b57905c6f849e8cd855d72947
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-10-20 15:04:34 -0700

    Refactor for voxpuplo/iis

--- site/profile/manifests/iis/baseline.pp
+++ site/profile/manifests/iis/baseline.pp
@@ -2,49 +2,48 @@ class profile::iis::baseline (
   $root_iis_path  = hiera('profile::iis::baseline::root_iis_path','C:/inetpub'),
 ) {
 
+  $dotnet_cloudshop_server = [ 'NET-Framework-45-Core', 'NET-Framework-45-ASPNET', 'AS-NET-Framework']
+
+  $iis_cloudshop_server =  ['Web-Server',
+                            'Application-Server',
+                            'AS-Web-Support',
+                            'Web-Mgmt-Tools',
+                            'Web-Mgmt-Console',
+                            'Web-Scripting-Tools',
+                            'Web-WebServer',
+                            'Web-App-Dev',
+                            'Web-Asp-Net45',
+                            'Web-ISAPI-Ext',
+                            'Web-ISAPI-Filter',
+                            'Web-Net-Ext45',
+                            'Web-Common-Http',
+                            'Web-Default-Doc',
+                            'Web-Dir-Browsing',
+                            'Web-Http-Errors',
+                            'Web-Http-Redirect',
+                            'Web-Static-Content',
+                            'Web-Health',
+                            'Web-Http-Logging',
+                            'Web-Log-Libraries',
+                            'Web-Request-Monitor',
+                            'Web-Stat-Compression',
+                            'Web-Dyn-Compression',
+                            'Web-Security',
+                            'Web-Basic-Auth',
+                            'Web-Digest-Auth',
+                            'Web-Filtering',
+                            'Web-IP-Security',
+                            'Web-Url-Auth',
+                            'Web-Windows-Auth']
+
   # Install Dot Net 4.5 first
-  windowsfeature { 'DOTNET_CLOUDSHOP_SERVER':
-    feature_name => [
-      'NET-Framework-45-Core',
-      'NET-Framework-45-ASPNET',
-      'AS-NET-Framework',
-    ]
+  windowsfeature { $dotnet_cloudshop_server:
+    ensure => present,
   } ->
+
   # Install IIS and dependent features
   windowsfeature { 'IIS_CLOUDSHOP_SERVER':
-    feature_name => [
-      'Web-Server',
-      'Application-Server',
-      'AS-Web-Support',
-      'Web-Mgmt-Tools',
-      'Web-Mgmt-Console',
-      'Web-Scripting-Tools',
-      'Web-WebServer',
-      'Web-App-Dev',
-      'Web-Asp-Net45',
-      'Web-ISAPI-Ext',
-      'Web-ISAPI-Filter',
-      'Web-Net-Ext45',
-      'Web-Common-Http',
-      'Web-Default-Doc',
-      'Web-Dir-Browsing',
-      'Web-Http-Errors',
-      'Web-Http-Redirect',
-      'Web-Static-Content',
-      'Web-Health',
-      'Web-Http-Logging',
-      'Web-Log-Libraries',
-      'Web-Request-Monitor',
-      'Web-Stat-Compression',
-      'Web-Dyn-Compression',
-      'Web-Security',
-      'Web-Basic-Auth',
-      'Web-Digest-Auth',
-      'Web-Filtering',
-      'Web-IP-Security',
-      'Web-Url-Auth',
-      'Web-Windows-Auth',
-    ]
+    ensure => present,
   } ->
   # Stop the Default Website
   dsc_xwebsite { 'DefaultSite':
