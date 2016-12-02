cdc8398fe0a3c58e2e5217ef51451ae65f8e0301
Carl Caum <carl@carlcaum.com>
2016-12-02 10:44:32 -0800

    Move role::appserver to role::rgbank::web

--- site/role/manifests/rgbank/web.pp
+++ site/role/manifests/rgbank/web.pp
@@ -1,6 +1,12 @@
-class role::rgbank::web {
-  include profile::apache
-  include profile::docker
+class role::rgbank::web(
+  $use_puppetconf_header = false
+) {
+  include profile::common
+  include profile::apache::remove
+  include profile::nginx
   include profile::mysql::client
-  include profile::rgbank::web
+  include profile::git
+
+  Class['profile::common'] -> Class['profile::nginx']
+  Class['profile::apache::remove'] -> Class['profile::nginx']
 }
