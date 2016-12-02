975c57c06642a9621d323a1bfe160a0d568adc39
Carl Caum <carl@carlcaum.com>
2016-12-02 10:23:49 -0800

    Use proper override_environment parameter for environment node groups

--- site/profile/manifests/node_groups/environments.pp
+++ site/profile/manifests/node_groups/environments.pp
@@ -1,26 +1,26 @@
 class profile::node_groups::environments {
 	node_group { 'Production environment':
-		ensure             => 'present',
-    environment_trumps => true,
-		environment        => 'production',
-		rule               => ['and', ['=', ['trusted', 'extensions', 'environment'], 'production']],
-		parent             => 'All Nodes',
+		ensure               => 'present',
+    override_environment => true,
+		environment          => 'production',
+		rule                 => ['and', ['=', ['trusted', 'extensions', 'environment'], 'production']],
+		parent               => 'All Nodes',
 	}
 
 	node_group { 'Staging environment':
-		ensure             => 'present',
-		environment        => 'staging',
-    environment_trumps => true,
-		rule               => ['and', ['=', ['trusted', 'extensions', 'environment'], 'staging']],
-		parent             => 'Production environment',
+		ensure               => 'present',
+		environment          => 'staging',
+    override_environment => true,
+		rule                 => ['and', ['=', ['trusted', 'extensions', 'environment'], 'staging']],
+		parent               => 'Production environment',
 	}
 
 	node_group { 'Dev environment':
-		ensure             => 'present',
-		environment        => 'dev',
-    environment_trumps => true,
-		rule               => ['and', ['=', ['trusted', 'extensions', 'environment'], 'dev']],
-		parent             => 'Staging environment',
+		ensure               => 'present',
+		environment          => 'dev',
+    override_environment => true,
+		rule                 => ['and', ['=', ['trusted', 'extensions', 'environment'], 'dev']],
+		parent               => 'Staging environment',
 	}
 
 }
