466f565ce667d3062f220b969e49201e36d73c5b
Carl Caum <carl@carlcaum.com>
2017-05-19 14:07:49 -0700

    Update nginx server_names_hash_bucket_size parameter

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -2,7 +2,11 @@ class profile::nginx {
   class { 'php': 
     composer => false,
   }
-  class { 'nginx': }
+  class { 'nginx': 
+    http_cfg_append => {
+      server_names_hash_bucket_size => 128,
+    },
+  }
 
   Class['php'] -> Class['nginx']
 }
