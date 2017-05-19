0a2de6d17c0799abb0266d155fbc548fcece3896
Carl Caum <carl@carlcaum.com>
2017-05-18 20:31:12 -0700

    Use Puppet 4 functions

--- site/profile/lib/puppet/functions/array2hash.rb
+++ site/profile/lib/puppet/functions/array2hash.rb
@@ -2,28 +2,13 @@
 # array2hash.rb
 #
 
-module Puppet::Parser::Functions
-  newfunction(:array2hash, :type => :rvalue, :doc => <<-EOS
-This converts any array to a hash.
-For example: [ [a,b], [c,d] ] => {a => b, c => d}
-Another example: [a, b, c, d] => {a => b, c => d}
-    EOS
-  ) do |arguments|
+Puppet::Functions.create_function(:to_hash) do
+  dispatch :to_hash do
+    param 'Array', :array
+  end
 
-    if arguments.empty?
-        return []
-    end
-
-    if arguments.length == 1
-        if arguments[0].kind_of?(Array)
-            return Hash[*arguments[0].flatten]
-        else
-            raise "The array2hash method only accepts arrays"
-        end
-    end
-
-    return arguments
+  def to_hash(array) do
+    Hash[*array.flatten]
   end
 end
-
 # vim: set ts=2 sw=2 et :
