0f89918359c81357310bea8bbccfbc107e0112bc
Carl Caum <carl@carlcaum.com>
2017-06-03 09:30:13 -0700

    Revert role::rgbank::web class to not use docker image

--- site/role/manifests/rgbank/web.pp
+++ site/role/manifests/rgbank/web.pp
@@ -1,5 +1,12 @@
 class role::rgbank::web(
   $use_puppetconf_header = false
 ) {
-  include rgbank::web::docker::image
+  include profile::common
+  include profile::apache::remove
+  include profile::nginx
+  include profile::mysql::client
+  include profile::git
+  
+  Class['profile::common'] -> Class['profile::nginx']
+  Class['profile::apache::remove'] -> Class['profile::nginx']
 }
