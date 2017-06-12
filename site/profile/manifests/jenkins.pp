be502184e5410feeb7f0e62240015f1ed84de538
Carl Caum <carl@carlcaum.com>
2017-06-11 23:33:56 -0700

    update jenkins plugins

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -5,29 +5,35 @@ class profile::jenkins {
   }
 
   #jenkins::job { "control-repo":
-  #  ensure  => present,
-  #  enabled => true,
-  #  config  => epp('profile/jenkins_job_config.epp'),
+  #  ensure                                               => present,
+  #  enabled                                              => true,
+  #  config                                               => epp('profile/jenkins_job_config.epp'),
   #}
 
-  jenkins::plugin { 'puppet-enterprise-pipeline': }
-  jenkins::plugin { 'build-pipeline-plugin': }
-  jenkins::plugin { 'plain-credentials': }
-  jenkins::plugin { 'workflow-basic-steps': }
-  jenkins::plugin { 'workflow-aggregator': }
   jenkins::plugin { 'workflow-scm-step': }
-  jenkins::plugin { 'workflow-support': }
-  jenkins::plugin { 'workflow-api': }
-  jenkins::plugin { 'scm-api': }
-  jenkins::plugin { 'ace-editor': }
-  jenkins::plugin { 'jquery-detached': }
-  jenkins::plugin { 'script-security': }
-  jenkins::plugin { 'workflow-cps': }
   jenkins::plugin { 'workflow-step-api': }
+  jenkins::plugin { 'workflow-support': }
+  jenkins::plugin { 'ace-editor': }
+  jenkins::plugin { 'bouncycastle-api': }
+  jenkins::plugin { 'display-url-api': }
+  jenkins::plugin { 'durable-task': }
+  jenkins::plugin { 'jquery-detached': }
+  jenkins::plugin { 'mailer': }
+  jenkins::plugin { 'scm-api': }
+  jenkins::plugin { 'credentials': version => '2.1.13', }
+  jenkins::plugin { 'script-security': version => '1.28', }
+  jenkins::plugin { 'plain-credentials': version => '1.4', }
+  jenkins::plugin { 'structs': version => '1.7', }
+  jenkins::plugin { 'workflow-api': version => '2.17', }
+  jenkins::plugin { 'workflow-basic-steps': version => '2.5', }
+  jenkins::plugin { 'workflow-cps': version  => '2.17', }
+  jenkins::plugin { 'workflow-durable-task-step': version =>  '2.4', }
+
   jenkins::plugin { 'copyartifact': }
   jenkins::plugin { 'matrix-project': }
   jenkins::plugin { 'git': }
   jenkins::plugin { 'ssh-agent': }
+  jenkins::plugin { 'puppet-enterprise-pipeline': }
 
   file {'/var/www/html/builds':
     ensure  => directory,
